c0efc1880e0dcde95a3d7674169cf64578bab60a
Israel Akinoso <akinosoissy@gmail.com>
2025-04-25 03:40:59 +0100

    Section 3.6 : Arrays

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,2 @@
-let sales: number = 123_456_789;
-let course: string = "TypScript";
-let is_published: boolean = true;
-let level;
-
-function render(document) {
-  console.log(document);
-}
+let numbers: number[] = [];
+numbers.forEach(n => n.toString())
