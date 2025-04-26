850c62d8ae997f9fa342cc10561e43bc4f97a792
Israel Akinoso <akinosoissy@gmail.com>
2025-04-26 14:45:26 +0100

    Section 4.1: Type aliases

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,10 @@
-// * Option 1
-// let employee: {
-//   id: number;
-//   name: string;
-// } = { id: 1, name: "" };
-
-// * Option 2
-// let employee: {
-//   id: number;
-//   name: string;
-// } = { id: 1, name: "Mosh" };
-
-// * Making property readonly
-// let employee: {
-//     readonly id: number;
-//     name: string;
-//   } = { id: 1, name: "Mosh" };
-
-// * Methods
-let employee: {
+type Employee = {
   id: number;
   name: string;
   retire: (date: Date) => void;
-} = {
+};
+
+let employee: Employee = {
   id: 1,
   name: "Mosh",
   retire: (date: Date) => {
